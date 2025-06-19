7941d028077a8a8699dedc5997533c1d7d1ef511
Josem1801 <Josemanuelrosales2@gmail.com>
2025-06-19 15:41:17 -0600

    feat(react): add Level component

---
+++ js/react/lib/components/level/level.const.ts
@@ -0,0 +1,15 @@
+export const LEVEL_VARIANTS = {
+  n1: "bg-primary-100",
+  n2: "bg-primary-300",
+  n3: "bg-primary-500",
+  op: "bg-secondary-400",
+};
+
+export const LEVEL_LABELS = {
+  n1: "N1",
+  n2: "N2",
+  n3: "N3",
+  op: "Op",
+};
+
+export type LevelVariants = keyof typeof LEVEL_VARIANTS;
