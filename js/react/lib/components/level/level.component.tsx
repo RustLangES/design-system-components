f93cf854c7c55faed32e5382d68d9161377f1240
Josem1801 <Josemanuelrosales2@gmail.com>
2025-06-19 15:49:23 -0600

    refactor: create withAs HOC to support dynamic tag rendering

--- js/react/lib/components/level/level.component.tsx
+++ js/react/lib/components/level/level.component.tsx
@@ -1,31 +1,26 @@
-import { ComponentPropsWithoutRef } from "react";
+import { withAs } from "../../utils/hoc/with-as.hoc";
 import { cn } from "../../utils/tw-merge";
 import { LEVEL_LABELS, LEVEL_VARIANTS, LevelVariants } from "./level.const";
 
-type LevelProps<C extends React.ElementType> = {
+type LevelProps = {
   variant: LevelVariants;
   className?: string;
-  as?: C;
-} & Omit<ComponentPropsWithoutRef<C>, "children">;
-
-export const Level = <C extends React.ElementType = "span">({
-  as,
-  variant,
-  className,
-  ...rest
-}: LevelProps<C>) => {
-  const Component = as || "span";
-  return (
-    <Component
-      className={cn([
-        "rounded-xl border border-black px-2 leading-[150%]",
-        "desktop:text-sm text-xxs",
-        LEVEL_VARIANTS[variant],
-        className,
-      ])}
-      {...rest}
-    >
-      {LEVEL_LABELS[variant]}
-    </Component>
-  );
 };
+
+export const Level = withAs(
+  (Component, { variant, className, ...rest }: LevelProps) => {
+    return (
+      <Component
+        {...rest}
+        className={cn([
+          "rounded-xl border border-black px-2 leading-[150%]",
+          "desktop:text-sm text-xxs",
+          LEVEL_VARIANTS[variant],
+          className,
+        ])}
+      >
+        {LEVEL_LABELS[variant]}
+      </Component>
+    );
+  }
+);
