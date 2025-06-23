e9adc29060ae2332a15378a0c4a3289463197fce
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-06-23 01:38:05 -0400

    feat(leptos): add radio component

--- crates/leptos/src/lib.rs
+++ crates/leptos/src/lib.rs
@@ -2,10 +2,17 @@ pub(crate) use tailwind_fuse::tw_merge as tw;
 
 pub mod avatar;
 pub mod button;
+pub mod radio;
 pub mod tag;
+
+pub mod inputs {
+    pub use crate::radio::Radio;
+}
 
 pub mod prelude {
     pub use crate::avatar::Avatar;
     pub use crate::button::{Button, Variant as ButtonVariant};
     pub use crate::tag::Tag;
+
+    pub use crate::radio::Radio as InputRadio;
 }
