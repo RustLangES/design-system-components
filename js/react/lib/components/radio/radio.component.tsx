62c443ee891b0af4ab4db339753d98d9fdfefdd3
Brayan-724 <bjqf0724@gmail.com>
2025-06-24 13:45:32 -0700

    refactor(styles): Migrate Radio from ReactJS

--- js/react/lib/components/radio/radio.component.tsx
+++ js/react/lib/components/radio/radio.component.tsx
@@ -6,15 +6,7 @@ type RadioProps = InputHTMLAttributes<HTMLInputElement>;
 export const Radio = ({ className, ...rest }: RadioProps) => {
   return (
     <input
-      className={cn([
-        "shadow-rb-black aspect-square appearance-none transition",
-        "flex size-4 items-center justify-center rounded-full border border-black",
-        "after:absolute after:size-2 after:rounded-full after:transition",
-        "dark:bg-dark bg-white",
-        "after:bg-gray dark:after:bg-neutral-500",
-        "checked:after:bg-primary-500",
-        className,
-      ])}
+      className={cn(["rustlanges-radio", className])}
       type="radio"
       {...rest}
     />
