0e4326283bcf828f7f29f45ab3e0a7e3769088ce
Brayan-724 <bjqf0724@gmail.com>
2025-07-01 15:22:09 -0700

    remove unused file

--- js/react/lib/components/card/card.types.ts
+++
@@ -1,8 +0,0 @@
-import { CARD_GROUP_VARIANTS } from "./card.const";
-
-export interface CardGroupProps {
-  variant?: keyof typeof CARD_GROUP_VARIANTS;
-  disabled?: boolean;
-  children: React.ReactNode;
-  className?: string;
-}
