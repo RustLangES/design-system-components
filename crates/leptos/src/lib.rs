14cd0929162d5c6de4a8fe84a0079ff257fa148d
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-07-09 01:01:04 -0400

    feat(leptos): add flaps

--- crates/leptos/src/lib.rs
+++ crates/leptos/src/lib.rs
@@ -2,6 +2,7 @@ pub(crate) use tailwind_fuse::tw_merge as tw;
 
 pub mod avatar;
 pub mod button;
+pub mod flap;
 pub mod icons;
 pub mod radio;
 pub mod tag;
@@ -13,6 +14,7 @@ pub mod inputs {
 pub mod prelude {
     pub use crate::avatar::Avatar;
     pub use crate::button::{Button, Variant as ButtonVariant};
+    pub use crate::flap::Flap;
     pub use crate::tag::Tag;
 
     pub use crate::radio::Radio as InputRadio;
