d907a452e976883fe03f89536f8a893400fc67d5
Brayan-724 <bjqf0724@gmail.com>
2025-07-11 16:32:50 -0700

    first setup of vue

---
+++ js/vue/eslint.config.ts
@@ -0,0 +1,30 @@
+import { globalIgnores } from "eslint/config";
+import { type ConfigArray } from "typescript-eslint";
+import globals from "globals";
+import js from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+
+export default defineConfigWithVueTs([
+  js.configs.recommended,
+  globalIgnores(["**/dist"]),
+  pluginVue.configs["flat/essential"],
+  vueTsConfigs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+
+    // plugins: { "react-refresh": reactRefresh },
+
+    rules: {
+      "no-sparse-arrays": "off",
+      "@typescript-eslint/no-unsafe-function-type": "off",
+    },
+  },
+]) as ConfigArray;
