749751d95f664ab52c71ed4ab81143cccd9b7bca
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2025-08-02 22:41:51 -0400

    fix(rust): publish crates

--- crates/leptos/src/button.rs
+++ crates/leptos/src/button.rs
@@ -15,14 +15,13 @@ pub fn Button(
     #[prop(into, optional)] variant: Variant,
     #[prop(into, optional)] label: String,
     #[prop(into, optional)] class: String,
-    attrs: Vec<(&'static str, Attribute)>,
     icon: View<impl IntoView + Send + Sync>,
 ) -> impl IntoView {
     let var = format!("{}{variant:?}", concat!(BASE_CLASS, "-button", "--"));
     let class = crate::tw!("text-button", var, concat!(BASE_CLASS, "-button"), class);
 
     view! {
-        <button class={class} {..attrs}>
+        <button class={class}>
             {(variant != Variant::Icon).then_some(label)}
             {(variant == Variant::Icon).then(|| icon.into_view())}
         </button>
