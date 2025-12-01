a4678e359533395cf25f7089f785ede4e921504d
Phosphorus-M <fernando.n.p.d.moscu@gmail.com>
2025-12-01 17:01:27 -0300

    fix: enhance Card component styling

--- crates/leptos/src/card.rs
+++ crates/leptos/src/card.rs
@@ -28,8 +28,8 @@ pub fn Card(
 
         return view! {
             <div class={class}>
-                <svg viewBox="0 0 334 444" fill="none" xmlns="http://www.w3.org/2000/svg" class="drop-shadow-resource-card">
-                    <path d="M267.221 2C275.589 2 283.519 5.74306 288.838 12.2038L325.617 56.8794C329.744 61.892 332 68.1829 332 74.6756V414C332 429.464 319.464 442 304 442H30C14.536 442 2 429.464 2 414V30C2 14.536 14.536 2 30 2H267.221Z" fill="#3D3D3D" stroke="black" stroke-width="2"/>
+                <svg viewBox="0 0 334 444" fill="none" xmlns="http://www.w3.org/2000/svg" class="text-primary-50 dark:text-neutral-900">
+                    <path d="M267.221 2C275.589 2 283.519 5.74306 288.838 12.2038L325.617 56.8794C329.744 61.892 332 68.1829 332 74.6756V414C332 429.464 319.464 442 304 442H30C14.536 442 2 429.464 2 414V30C2 14.536 14.536 2 30 2H267.221Z" fill="currentColor" stroke-width="2"/>
                 </svg>
                 <div class="rustlanges-card--resource-body">
                 {children()}
