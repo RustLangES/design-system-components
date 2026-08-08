4df8bbc5779a018cefc54457250194e2e827549f
MarioYellowy <mario03eche@gmail.com>
2026-08-08 13:36:42 -0600

    feat(flake): Remove specific version 22, delegate the latest stable in the channel

--- flake.nix
+++ flake.nix
@@ -1,16 +1,19 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        # js
-        nodejs_22
-        pnpm_10
-      ];
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # js
+          nodejs
+          pnpm_10
+        ];
+      };
     };
-  };
 }
