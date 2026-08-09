b5b8310d0c9e7013d648b9eea4d405a28c3e00ff
MarioYellowy <mario03eche@gmail.com>
2026-08-09 13:56:25 -0600

    feat(flake): Add node, pnpm and rust toolchain configuration according to the flake from the web official

--- flake.nix
+++ flake.nix
@@ -1,19 +1,48 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  description = "Design systems components for RustLangEs web flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # js
-          nodejs
-          pnpm_10
-        ];
-      };
-    };
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust toolchain with wasm target
+            rustToolchain
+
+            # WASM tools
+            tailwindcss_4
+            wasm-bindgen-cli_0_2_126
+
+            # Leptos specific tools
+            leptosfmt
+            cargo-leptos
+
+            # Additional development tools
+            pkg-config
+            openssl
+            pnpm
+            nodejs_24
+          ];
+        };
+      }
+    );
 }
