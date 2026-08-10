0e11d500594c1bee539464adde96dacca645d19b
MarioYellowy <mario03eche@gmail.com>
2026-08-10 12:37:58 -0600

    fix(vue): Add parser options

--- js/vue/eslint.config.ts
+++ js/vue/eslint.config.ts
@@ -15,6 +15,12 @@ export default defineConfigWithVueTs([
   vueTsConfigs.recommended,
   {
     languageOptions: {
+
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+
       globals: {
         ...globals.browser,
       },
